81c10cbdce9656d4be6bb944074cbc78c90113ca
￴ <>
2023-02-09 20:39:27 +0700

    udpate

--- lib/galaxeus_lib_flutter.dart
+++ lib/galaxeus_lib_flutter.dart
@@ -1,4 +1,21 @@
 library galaxeus_lib_flutter;
 
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
 export "extension/extension.dart";
 export "widget/widget.dart";
+
+class MyScrollBehavior extends MaterialScrollBehavior {
+
+  @override
+  Set<PointerDeviceKind> get dragDevices {
+    return {
+      PointerDeviceKind.touch,
+      PointerDeviceKind.mouse,
+      PointerDeviceKind.trackpad,
+      PointerDeviceKind.stylus,
+      PointerDeviceKind.invertedStylus,
+    };
+  }
+}
